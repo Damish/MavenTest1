5440732984dc5c725eccd24c9528bb7c51584163
Damish <damishs88@gmail.com>
2021-03-29 18:31:48 +0530

    initial

---
+++ src/main/java/com/example/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,32 @@
+package com.example.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+
+	// GET
+	// URI - /hello-world
+	// Method -"Hello world"
+
+	@GetMapping(path = "/hello-world")
+	public String helloworld() {
+		return "Hello World";
+	}
+
+	// URI - /hello-world-bean
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+
+	// URI - /hello-world/path-variable/in28minutes
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldBean(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World ,%s",name));
+	} 
+
+}
